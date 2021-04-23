9d45558503d247246680b0e77b7ca91d3824e6b3
avthorn <alex.vincent.thorn@outlook.com>
2021-04-23 09:44:14 +0000

    Added template 4

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -11,21 +11,21 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+borovecki_data_clean_aug <- read_tsv(file = "data/03_borovecki_data_clean_aug.tsv.gz")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+#borovecki_data_clean_aug %>% ...
 
 
 # Model data
-my_data_clean_aug %>% ...
+#borovecki_data_clean_aug %>% ...
 
 
 # Visualise data ----------------------------------------------------------
-my_data_clean_aug %>% ...
+#borovecki_data_clean_aug %>% ...
 
 
 # Write data --------------------------------------------------------------
-write_tsv(...)
-ggsave(...)
+#write_tsv(...)
+#ggsave(...)
