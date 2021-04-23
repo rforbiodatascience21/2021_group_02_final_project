e3e6a754262b57660959bf9123af5ca4cc3c0eaf
EmilieBruun <s173703@student.dtu.dk>
2021-04-23 10:56:37 +0000

    merge conflict solved

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,10 +2,6 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-<<<<<<< HEAD
-source(file = "R/04_analysis_i.R")
-
-=======
 source(file = "R/04_analysis_1.R")
 source(file = "R/04_analysis_2.R")
->>>>>>> a6d423fef10864a7d4c762c1a7eadf737f1f0273
+
