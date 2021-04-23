85c25b2e8750e3e40b2e43d4943bd07d68359778
avthorn <alex.vincent.thorn@outlook.com>
2021-04-23 11:17:47 +0000

    Fixed names

--- R/03_augment.R
+++ R/03_augment.R
@@ -11,27 +11,30 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean <- read_tsv(file = "data/02_borovecki_data_clean.tsv")
+borovecki_data_clean <- read_tsv(file = "data/02_borovecki_data_clean.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug <- my_data_clean 
-#%>%
-#  mutate(outcome = case_when(value == "good" ~ 0,
-#                             value == "poor" ~ 1)) %>%
-#  relocate("outcome")
-
-
-# Write data --------------------------------------------------------------
-write_tsv(x = my_data_clean_aug,
-          path = "data/03_borovecki_data_clean_aug.tsv")
 
 borovecki_data_clean_aug_all_genes <- borovecki_data_clean  %>%
   relocate(outcome)
-  
+
 borovecki_data_clean_aug_marker_genes <- borovecki_data_clean_aug_all_genes  %>%
   select("outcome", "201012_at", "202653_s_at", "208374_s_at", "200989_at", "212287_at", 
          "218589_at", "217816_s_at", "213044_at", 
          "201071_x_at", "213168_at", "201023_at", "217783_s_at")
+
+
+
+
+# Write data --------------------------------------------------------------
+write_tsv(x = borovecki_data_clean_aug_all_genes,
+          path = "data/03_borovecki_data_clean_aug_all_genes.tsv")
+
+write_tsv(x = borovecki_data_clean_aug_marker_genes,
+          path = "data/03_borovecki_data_clean_aug_marker_genes.tsv")
+
+  
+
 
 
