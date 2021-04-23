1fd88f8fc0483232403623fc91db3beedf0aaa84
AstaLaugesen <asta.mimitia@gmail.com>
2021-04-23 09:50:41 +0000

    add more analyses

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,4 +2,5 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-source(file = "R/04_analysis_i.R")
+source(file = "R/04_analysis_1.R")
+source(file = "R/04_analysis_2.R")
