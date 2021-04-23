9e4786fdbdb4c006bf293e4cdda279b530514039
AstaLaugesen <asta.mimitia@gmail.com>
2021-04-23 09:52:22 +0000

    add analyses

---
+++ R/04_analysis_2.R
@@ -0,0 +1,31 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+borovecki_data_clean_aug <- read_tsv(file = "data/03_borovecki_data_clean_aug.tsv.gz")
+
+
+# Wrangle data ------------------------------------------------------------
+#borovecki_data_clean_aug %>% ...
+
+
+# Model data
+#borovecki_data_clean_aug %>% ...
+
+
+# Visualise data ----------------------------------------------------------
+#borovecki_data_clean_aug %>% ...
+
+
+# Write data --------------------------------------------------------------
+#write_tsv(...)
+#ggsave(...)
