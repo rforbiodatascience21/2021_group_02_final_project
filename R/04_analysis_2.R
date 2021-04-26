911fe71a00b3bffb132530804dae718a99e6ab51
avthorn <alex.vincent.thorn@outlook.com>
2021-04-26 09:13:15 +0000

    Improved PCA

--- R/04_analysis_2.R
+++ R/04_analysis_2.R
@@ -27,23 +27,22 @@ borovecki_data_clean_aug_all_genes <- read_tsv(file = "data/03_borovecki_data_cl
 
 # Model data
 
+pca_fit <- function(data){
+  return(data %>% 
+           select(where(is.numeric)) %>% # retain only numeric columns
+           scale() %>% # scale data
+           prcomp())
+}
 
-pca_fit_m <- borovecki_data_clean_aug_marker_genes %>% 
-  select(where(is.numeric)) %>% # retain only numeric columns
-  scale() %>% # scale data
-  prcomp() # do PCA
+pca_fit_m <- pca_fit(borovecki_data_clean_aug_marker_genes) 
 
-
-pca_fit_a <- borovecki_data_clean_aug_all_genes %>% 
-  select(where(is.numeric)) %>% # retain only numeric columns
-  scale() %>% # scale data
-  prcomp() # do PCA
+pca_fit_a <- pca_fit(borovecki_data_clean_aug_all_genes)
 
 # Visualise data ----------------------------------------------------------
 
 # Plot PCA
 
-pca_fit_m %>%
+pca_fit_m %>% 
   augment(borovecki_data_clean_aug_marker_genes) %>% # add original dataset back in
   ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
   geom_point(size = 1.5) +
@@ -61,9 +60,9 @@ pca_fit_a %>%
   ) +
   theme_half_open(12) + background_grid()
 
----
+# Plot Variance explained by each principal component
   
-  pca_fit_m %>%
+pca_fit_m %>%
   tidy(matrix = "eigenvalues") %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#56B4E9", alpha = 0.8) +
@@ -85,31 +84,6 @@ pca_fit_a %>%
   ) +
   theme_minimal_hgrid(12)
 
----
-# plot rotation matrix for marker genes
-  
-arrow_style <- arrow(
-  angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
-)
-
-
-pca_fit_m %>%
-  tidy(matrix = "rotation") %>%
-  pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
-  ggplot(aes(PC1, PC2)) +
-  geom_segment(xend = 0, yend = 0, arrow = arrow_style) +
-  geom_text(
-    aes(label = column),
-    hjust = 1, nudge_x = -0.02, 
-    color = "#904C2F"
-  ) +
-  xlim(-1.25, .5) + ylim(-.5, 1) +
-  coord_fixed() + # fix aspect ratio to 1:1
-  theme_minimal_grid(12)
-
-
-
----
 
 
 
@@ -119,6 +93,4 @@ pca_fit_m %>%
 
 
 
-# Write data --------------------------------------------------------------
-#write_tsv(...)
-#ggsave(...)
+
