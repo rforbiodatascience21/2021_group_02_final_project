43a23deec9d6eafca36d5e2d74fcab40277b10c6
avthorn <alex.vincent.thorn@outlook.com>
2021-04-26 09:29:00 +0000

    More PCA stuff

--- R/04_analysis_2.R
+++ R/04_analysis_2.R
@@ -1,12 +1,10 @@
-## PCA
-
 # Clear workspace ---------------------------------------------------------
 rm(list = ls())
 
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
-library(broom)
+library(broom)  # devtools::install_github("tidymodels/broom")
 library(cowplot)
 
 
@@ -14,9 +12,11 @@ library(cowplot)
 source(file = "R/99_project_functions.R")
 
 
+
+
 # Load data ---------------------------------------------------------------
 
-# Load marker gene dataset
+# Load marker genes dataset
 borovecki_data_clean_aug_marker_genes <- read_tsv(file = "data/03_borovecki_data_clean_aug_marker_genes.tsv")
 
 # Load all genes dataset
@@ -34,6 +34,8 @@ pca_fit <- function(data){
            prcomp())
 }
 
+
+
 pca_fit_m <- pca_fit(borovecki_data_clean_aug_marker_genes) 
 
 pca_fit_a <- pca_fit(borovecki_data_clean_aug_all_genes)
@@ -42,23 +44,28 @@ pca_fit_a <- pca_fit(borovecki_data_clean_aug_all_genes)
 
 # Plot PCA
 
-pca_fit_m %>% 
-  augment(borovecki_data_clean_aug_marker_genes) %>% # add original dataset back in
-  ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
-  geom_point(size = 1.5) +
-  scale_color_manual(
-    values = c(symptomatic = "#D55E00", pre_symptomatic = "#00FF00", control = "#0072B2")
-  ) +
-  theme_half_open(12) + background_grid()
+pca_plot <- function(data){
+  return(data %>% 
+           select(where(is.numeric)) %>% # retain only numeric columns
+           scale() %>% # scale data
+           prcomp() %>% 
+           augment(data) %>% # add original dataset back in
+           ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
+           geom_point(size = 1.5) +
+           scale_color_manual(
+             values = c(symptomatic = "#D55E00", pre_symptomatic = "#00FF00", control = "#0072B2")
+           ) +
+           theme_half_open(12) + background_grid())
+}
 
-pca_fit_a %>%
-  augment(borovecki_data_clean_aug_all_genes) %>% # add original dataset back in
-  ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
-  geom_point(size = 1.5) +
-  scale_color_manual(
-    values = c(symptomatic = "#D55E00", pre_symptomatic = "#00FF00", control = "#0072B2")
-  ) +
-  theme_half_open(12) + background_grid()
+
+
+
+pca_plot(borovecki_data_clean_aug_marker_genes)
+
+pca_plot(borovecki_data_clean_aug_all_genes)
+
+
 
 # Plot Variance explained by each principal component
   
