5f53320c85b9013d7331e023157d8e64380bf0ae
avthorn <alex.vincent.thorn@outlook.com>
2021-04-26 07:50:46 +0000

    PCA improved

--- R/04_analysis_2.R
+++ R/04_analysis_2.R
@@ -15,19 +15,35 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
+
+# Load marker gene dataset
 borovecki_data_clean_aug_marker_genes <- read_tsv(file = "data/03_borovecki_data_clean_aug_marker_genes.tsv")
+
+# Load all genes dataset
+borovecki_data_clean_aug_all_genes <- read_tsv(file = "data/03_borovecki_data_clean_aug_all_genes.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
 
+# Model data
 
-pca_fit <- borovecki_data_clean_aug_marker_genes %>% 
+
+pca_fit_m <- borovecki_data_clean_aug_marker_genes %>% 
   select(where(is.numeric)) %>% # retain only numeric columns
   scale() %>% # scale data
   prcomp() # do PCA
 
 
-pca_fit %>%
+pca_fit_a <- borovecki_data_clean_aug_all_genes %>% 
+  select(where(is.numeric)) %>% # retain only numeric columns
+  scale() %>% # scale data
+  prcomp() # do PCA
+
+# Visualise data ----------------------------------------------------------
+
+# Plot PCA
+
+pca_fit_m %>%
   augment(borovecki_data_clean_aug_marker_genes) %>% # add original dataset back in
   ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
   geom_point(size = 1.5) +
@@ -36,12 +52,48 @@ pca_fit %>%
   ) +
   theme_half_open(12) + background_grid()
 
+pca_fit_a %>%
+  augment(borovecki_data_clean_aug_all_genes) %>% # add original dataset back in
+  ggplot(aes(.fittedPC1, .fittedPC2, color = outcome)) + 
+  geom_point(size = 1.5) +
+  scale_color_manual(
+    values = c(symptomatic = "#D55E00", pre_symptomatic = "#00FF00", control = "#0072B2")
+  ) +
+  theme_half_open(12) + background_grid()
+
+---
+  
+  pca_fit_m %>%
+  tidy(matrix = "eigenvalues") %>%
+  ggplot(aes(PC, percent)) +
+  geom_col(fill = "#56B4E9", alpha = 0.8) +
+  scale_x_continuous(breaks = 1:9) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))
+  ) +
+  theme_minimal_hgrid(12)
+
+pca_fit_a %>%
+  tidy(matrix = "eigenvalues") %>%
+  ggplot(aes(PC, percent)) +
+  geom_col(fill = "#56B4E9", alpha = 0.8) +
+  scale_x_continuous(breaks = 1:9) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))
+  ) +
+  theme_minimal_hgrid(12)
+
+---
+# plot rotation matrix for marker genes
+  
 arrow_style <- arrow(
   angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
 )
 
-# plot rotation matrix
-pca_fit %>%
+
+pca_fit_m %>%
   tidy(matrix = "rotation") %>%
   pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
   ggplot(aes(PC1, PC2)) +
@@ -56,23 +108,15 @@ pca_fit %>%
   theme_minimal_grid(12)
 
 
-pca_fit %>%
-  tidy(matrix = "eigenvalues") %>%
-  ggplot(aes(PC, percent)) +
-  geom_col(fill = "#56B4E9", alpha = 0.8) +
-  scale_x_continuous(breaks = 1:9) +
-  scale_y_continuous(
-    labels = scales::percent_format(),
-    expand = expansion(mult = c(0, 0.01))
-  ) +
-  theme_minimal_hgrid(12)
 
-# Model data
-#borovecki_data_clean_aug %>% ...
+---
 
 
-# Visualise data ----------------------------------------------------------
-#borovecki_data_clean_aug %>% ...
+
+
+
+
+
 
 
 # Write data --------------------------------------------------------------
