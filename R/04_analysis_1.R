2980b8a39b4c66ab9bca495985fade94f2a02223
EmilieBruun <s173703@student.dtu.dk>
2021-04-30 13:22:46 +0000

    improved log2 fold change plot

--- R/04_analysis_1.R
+++ R/04_analysis_1.R
@@ -4,6 +4,8 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("ggrepel")
+
 
 
 # Define functions --------------------------------------------------------
@@ -39,31 +41,41 @@ symp_df <- borovecki_data_clean_aug_all_genes %>%
   select(Gene, Patient_sum, everything())
 symp_df
 
+marker_genes <- c("201012_at", "202653_s_at", "208374_s_at", "200989_at", 
+                  "212287_at", "218589_at", "217816_s_at", "213044_at", 
+                  "201071_x_at", "213168_at", "201023_at", "217783_s_at")
 
 # Merge the two dataframes and compute the log2 fold change 
 full_df <- full_join(symp_df, control_df, by = "Gene") %>%
   mutate(Log2_foldchange = log2(Patient_sum/Control_sum))  %>%
-  mutate(Significant_level =  case_when(Log2_foldchange >= 1.8 ~ "Significantly upregulated",
-                                        Log2_foldchange <= -1 ~ "Significantly downregulated",
+  mutate(Significant_level =  case_when(Log2_foldchange >= 1.8 ~ "Significantly up regulated",
+                                        Log2_foldchange <= -1 ~ "Significantly down regulated",
                                         TRUE ~ "Non-significant")) %>%
-  select(Gene, Patient_sum, Control_sum, Log2_foldchange, Significant_level, everything())
+  mutate(Marker_gene_name = case_when(is.element(Gene, marker_genes) ~ Gene,
+                                      TRUE ~ "")) %>%
+  select(Gene, Marker_gene_name, Patient_sum, Control_sum, Log2_foldchange, Significant_level, everything())
+full_df
 
 
 
 
-# Visualise data ----------------------------------------------------------
+# Visualize data ----------------------------------------------------------
 full_df %>%  
   ggplot(mapping = aes(x = Gene, 
                        y = Log2_foldchange,
-                       color = Significant_level)) +
+                       color = Significant_level,
+                       label = Marker_gene_name)) +
   geom_point() +
   geom_hline(yintercept = 1.8, color = "blue", size = 1) + 
   geom_hline(yintercept = -1, color = "blue", size = 1) + 
   labs(x = "Gene", 
        y = "Log2 fold change", 
-       title = "Log2-fold change of expression data") +
-  scale_colour_manual(name = "Significant level", values = c("black", "red", "green")) + 
-  theme(axis.text.x = element_blank())
+       title = "Log2 fold change of expression data") +
+  scale_colour_manual(name = "Significant level", values = c("grey54", "red", "green")) + 
+  theme(axis.text.x = element_blank()) +
+  #geom_point(aes(colour = Marker_gene_name))
+  #geom_text(aes(label = Marker_gene_name, color = "blue"), size=3.1) 
+  geom_text_repel(size = 3.1,  max.overlaps = Inf, color = "black")
 
 
 # Write data --------------------------------------------------------------
