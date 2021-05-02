e255770bdc74c21d237408e6f3e7164daa0a1303
EmilieBruun <s173703@student.dtu.dk>
2021-05-02 06:04:04 +0000

    correction; probe instead of gene

--- R/01_load.R
+++ R/01_load.R
@@ -13,7 +13,7 @@ source(file = "R/99_project_functions.R")
 
 # Load and wrangle data ---------------------------------------------------------------
 
-# Make tibble where first column is the gene shared between the patients
+# Make tibble where first column is the probe name shared between the patients
 borovecki_data <- dir(path = "data/raw", pattern = "*.tsv", full.names = T) %>%
   map(read_tsv) %>%
   reduce(inner_join, by = "ID_REF")
