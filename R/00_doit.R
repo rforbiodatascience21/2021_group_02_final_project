323aa957029347d6a76895f8345354978b8baf64
avthorn <alex.vincent.thorn@outlook.com>
2021-05-03 07:41:19 +0000

    Doit updated

--- R/00_doit.R
+++ R/00_doit.R
@@ -4,4 +4,7 @@ source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
 source(file = "R/04_analysis_1.R")
 source(file = "R/04_analysis_2.R")
+source(file = "R/04_analysis_3.R")
+source(file = "R/04_analysis_4.R")
+
 
