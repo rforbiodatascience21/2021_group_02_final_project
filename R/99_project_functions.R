c5725d6d49a9376b1c0682fe119f745f9fbab49c
EmilieBruun <s173703@student.dtu.dk>
2021-05-04 05:33:31 +0000

    add function to select marker genes, add them to large tibble and plot log2 fold change

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -9,6 +9,25 @@ long_log2 <- function(data){
            mutate(expression = log2(expression))) #Log transform expression values
 }
 
+# Find marker genes
+find_marker_genes <- function(data, significant_level){
+  return(data %>%
+           filter(Log2_foldchange > significant_level) %>%
+           pull(Gene))
+}
+
+
+# Add marker genes to tibble
+add_marker_genes_to_tibble <- function(data, marker_genes){
+  return(data %>%
+           mutate(Marker_gene = case_when(is.element(Gene, marker_genes) ~ "1",
+                                          TRUE ~ "0")) %>%
+           mutate(Marker_gene_name = case_when(is.element(Gene, marker_genes) ~ Gene,
+                                               TRUE ~ "")) %>%
+           select(Gene, Marker_gene, Marker_gene_name, Patient_mean, Control_mean, 
+                  Log2_foldchange, Significant_level, everything()))
+} 
+
 
 
 #MODELLING FUNCTIONS------------------------------------------------------------
@@ -33,6 +52,33 @@ kmeans_func <- function(data){
 
 
 #PLOTTING FUNCTIONS-------------------------------------------------------------
+
+# Log2 fold change plot
+log2_fold_change_plot <- function(data, plot_title){
+  return(data %>%
+           ggplot(mapping = aes(x = Gene, 
+                              y = Log2_foldchange,
+                              color = Significant_level,
+                              shape = Marker_gene)) +
+           geom_point(size = 2) +
+           geom_hline(yintercept = 1.8, color = "blue", size = 1) + 
+           geom_hline(yintercept = -1, color = "blue", size = 1) + 
+           labs(x = "Gene", 
+                y = "Log2 fold change", 
+                title = plot_title,
+                shape = "Marker gene") +
+           scale_colour_manual(name = "Significant level", values = 
+                                 c("grey54", "red", "green3")) + 
+           theme(axis.text.x = element_blank(), axis.ticks = element_blank()) +
+           geom_label_repel(aes(
+                            label = Marker_gene_name), 
+                            nudge_y = -0.2, 
+                            size = 2.6, color = "black", 
+                            min.segment.length = unit(0, 'lines'), 
+                            max.overlaps = Inf))
+  
+}
+
 
 #Point plot of kmeans clusters with PC1 and PC2 on the axes
 kmeans_plot <- function(pca_fit_data, kmeans_data){
