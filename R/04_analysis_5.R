393812cfe8718969225175112b92497f583699cb
JulieKragmose <s171220@student.dtu.dk>
2021-05-10 15:40:08 +0000

    Removed kmeans plot for all genes

--- R/04_analysis_5.R
+++ R/04_analysis_5.R
@@ -19,20 +19,16 @@ source(file = "R/99_project_functions.R")
 # Load marker genes dataset
 borovecki_data_clean_aug_marker_genes <- read_tsv(file = "data/03_borovecki_data_clean_aug_marker_genes.tsv")
 
-# Load all genes dataset
-borovecki_data_clean_aug_all_genes <- read_tsv(file = "data/03_borovecki_data_clean_aug_all_genes.tsv")
-
 
 
 # Wrangle data ------------------------------------------------------------
 
-#Do kmeans of both marker and all genes
+#Do kmeans of marker genes
 kmeans_marker_genes_data <- kmeans_func(borovecki_data_clean_aug_marker_genes)
-kmeans_all_genes_data <- kmeans_func(borovecki_data_clean_aug_all_genes)
 
-#Do PCA fit of both marker and all genes
+#Do PCA fit of marker genes
 pca_fit_marker_genes_data <- pca_fit(borovecki_data_clean_aug_marker_genes)
-pca_fit_all_genes_data <- pca_fit(borovecki_data_clean_aug_all_genes)
+
 
 
 # Visualise data ----------------------------------------------------------
@@ -41,16 +37,10 @@ pca_fit_all_genes_data <- pca_fit(borovecki_data_clean_aug_all_genes)
 kmeans_marker_genes_plot <- kmeans_plot(pca_fit_marker_genes_data, kmeans_marker_genes_data) + 
   ggtitle("Marker genes")
 
-#Plot kmeans clustering for all genes
-kmeans_all_genes_plot <- kmeans_plot(pca_fit_all_genes_data, kmeans_marker_genes_data) + 
-  ggtitle("All genes")
-
-kmeans_plots <- kmeans_marker_genes_plot + kmeans_all_genes_plot +
-  plot_layout(guides = "collect")
 
 
 # Write data --------------------------------------------------------------
-ggsave(file = "Results/kmeans_plots.png", 
-       plot = kmeans_plots,
+ggsave(file = "Results/kmeans_plot.png", 
+       plot = kmeans_marker_genes_plot,
        width = 6.77, 
        height = 2.83)
